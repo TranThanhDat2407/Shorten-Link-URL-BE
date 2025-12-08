2520961552c8a2ff14bb63cdf1e4f39fc10ddaa5
TranThanhDat2407 <tranthanhdat2407.email@gmail.com>
2025-12-08 15:32:50 +0700

    fix CORS for front end 2

--- src/main/java/com/example/short_link/sercurity/config/SecurityConfig.java
+++ src/main/java/com/example/short_link/sercurity/config/SecurityConfig.java
@@ -115,9 +115,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of(frontEndDomain));
-        config.setAllowedMethods(List.of("*"));
+        config.setAllowedOrigins(List.of(frontEndDomain));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
+        config.setExposedHeaders(List.of("Authorization", "Set-Cookie"));
         config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
